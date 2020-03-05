10dfa40ef759740917ce7f18cef0749dfca1baa3
chelsea barocio <barocio.chelsea@gmail.com>
2020-03-05 01:28:15 -0800

    changed functions back to class based functions and removed hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const[personState, setPersonsState] = useState({
+
+class App extends Component {
+  state = {
     persons: [
      { name: 'Chelsea', age: 26 },
      { name: 'John', age: 29 },
      { name: 'Helen', age: 25 },
-   ]
-  });
+   ],
+   otherState: 'Some other value'
+  };
 
-  const [OtherState, setOtherState] = useState('some other value');
 
-  console.log(personState, OtherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    //console.log('this was clicked');
+    this.setState({
       persons: [
         { name: 'Chels', age: 26 },
         { name: 'John', age: 29 },
         { name: 'Hellz', age: 25 }
       ]
-      //because function does not merge with old state info, manually adding old state data below 
-      //OtherState: personState.OtherState
     });
  console.log('was clicked');
   };
 
+    render() {
      return (
       <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>this is working</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbies: Crafting</Person>
-      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Crafting</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       <Person/>
       </div>
     )
    //return React.createElement('div',{className: 'App'},React.createElement('h1', null, 'DOes this work now'));
+     }
 }
 
 export default App;
