1347ee4c78868ee4ddb90fb1c1fa62e51919f278
chelsea barocio <barocio.chelsea@gmail.com>
2020-03-05 17:57:33 -0800

    created person.css, added styling to Person component

--- src/App.js
+++ src/App.js
@@ -22,8 +22,18 @@ class App extends Component {
         { name: 'John', age: 29 },
         { name: 'Hellz', age: 25 }
       ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState ( {
+      persons: [
+        { name: 'Chelsea', age: 26 },
+        { name: event.target.value, age: 29 },
+        { name: 'Hellz', age: 25 }
+      ]
     });
- console.log('was clicked');
+    console.log('name change handler triggered');
   };
 
     render() {
@@ -32,17 +42,24 @@ class App extends Component {
       <h1>Hi, I'm a React App</h1>
       <p>this is working</p> 
       <button onClick={ () => this.switchNameHandler('CHEEEEEEELS') }>Switch Name</button>
+
       <Person 
       name={this.state.persons[0].name} 
       age={this.state.persons[0].age}/>
+
       <Person 
       name={this.state.persons[1].name} 
       age={this.state.persons[1].age}
-      click={this.switchNameHandler.bind(this, 'CHELSEA 2')} > My Hobbies: Crafting</Person>
+      click={this.switchNameHandler.bind(this, 'CHELSEA 2')} 
+      changed={this.nameChangedHandler} 
+      > My Hobbies: Crafting
+      </Person>
+
       <Person 
       name={this.state.persons[2].name} 
       age={this.state.persons[2].age}/>
       <Person/>
+
       </div>
     )
    //return React.createElement('div',{className: 'App'},React.createElement('h1', null, 'DOes this work now'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Person from './Person.css';
 
 const person = (props) => {
     return (
-        (<div>
+        (<div className ='Person'>
         <p onClick={props.click}> i'm {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
-        </div>)
+        <input type ="text" onChange={props.changed} value={props.name}/>
+        </div>
+        )
     )
 }
 
-export default person
+export default person;
 
